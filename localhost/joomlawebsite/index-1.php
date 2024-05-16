ec99112959176c84d1638c8a46de075479dd94a2
LosPadrinosxd <oscar.aleman080906@gmail.com>
2024-05-16 11:22:20 -0600

    Add files via upload

---
+++ localhost/joomlawebsite/index-1.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="es-es">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://localhost"/>
+	<id>http://localhost/joomlawebsite/index.php</id>
+	<updated>2024-05-16T17:20:38+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://localhost/joomlawebsite//joomlawebsite/index.php?format=feed&amp;type=atom"/>
+</feed>
